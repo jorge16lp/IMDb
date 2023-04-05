3480b3d9e558180237510f54cae20a1fb9f81571
jorge16lp <jor.lp.16@gmail.com>
2023-04-05 11:21:27 +0200

    typeScript in stores

--- src/store/search.module.ts
+++ src/store/search.module.ts
@@ -23,63 +23,63 @@ export const searchModule: Module<SearchState, any> = {
         directors: []
     }),
     mutations: {
-        setQuery(state, query) {
-            state.query = query
+        setQuery(state: SearchState, query: string): void {
+            state.query = query;
         },
-        resetFilters(state) {
+        resetFilters(state: SearchState): void {
             state.query = '';
             state.genders = [];
             state.releaseYear = 2000;
             state.actors = [];
         },
-        addGender(state: SearchState, gender) {
+        addGender(state: SearchState, gender: string): void {
             state.genders[state.genders.length] = gender;
         },
-        removeGender(state: SearchState, gender) {
+        removeGender(state: SearchState, gender: string): void {
             for (let i=0; i < state.genders.length; i++)
                 if (state.genders[i]===gender)
                     state.genders.splice(i, 1);
         },
-        setReleaseYear(state: SearchState, year) {
+        setReleaseYear(state: SearchState, year: string): void {
             state.releaseYear = parseInt(year);
         },
-        setDuration(state: SearchState, duration) {
+        setDuration(state: SearchState, duration: string): void {
             state.duration = parseInt(duration);
         },
-        addActor(state: SearchState, actor) {
+        addActor(state: SearchState, actor: string): void {
             state.actors[state.actors.length] = actor;
         },
-        removeActor(state: SearchState, actor) {
+        removeActor(state: SearchState, actor: string): void {
             for (let i=0; i < state.actors.length; i++)
                 if (state.actors[i]===actor)
                     state.actors.splice(i, 1);
         },
-        addDirector(state: SearchState, director) {
+        addDirector(state: SearchState, director: string): void {
             state.directors[state.directors.length] = director;
         },
-        removeDirector(state, director) {
+        removeDirector(state: SearchState, director: string): void {
             for (let i=0; i < state.directors.length; i++)
                 if (state.directors[i]===director)
                     state.directors.splice(i, 1);
         }
     },
     getters: {
-        getQuery(state) {
+        getQuery(state: SearchState): string {
             return state.query
         },
-        getGenders(state) {
+        getGenders(state: SearchState): string[] {
             return state.genders
         },
-        getReleaseYear(state) {
+        getReleaseYear(state: SearchState): number {
             return state.releaseYear
         },
-        getDuration(state) {
+        getDuration(state: SearchState): number {
             return state.duration
         },
-        getActors(state) {
+        getActors(state: SearchState): string[] {
             return state.actors
         },
-        getDirectors(state) {
+        getDirectors(state: SearchState): string[] {
             return state.directors
         }
     }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import {createStore} from "vuex";
-import {searchModule} from "@/store/search.module";
-import {charactersModule} from "@/store/characters.module";
+import {searchModule} from "./search.module";
+import {charactersModule} from "./characters.module";
 
 export const store = createStore({
     modules: {
